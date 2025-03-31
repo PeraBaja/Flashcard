5f563ede519d5619981d3a2df64840859b48bc34
Benjamín Peralta <benjaminp158@gmail.com>
2025-03-31 13:50:55 -0300

    Added functionality to controllers

--- Controllers/FlashcardController.cs
+++ Controllers/FlashcardController.cs
@@ -9,12 +9,24 @@ class FlashcardController(string dbConnection, StackController stackController)
     public void CreateTable()
     {
         _connection.Open();
-        _connection.Execute("CREATE TABLE IF NOT EXIST flashcard(id AUTOINCREMENT, name varchar(50))");
+        _connection.Execute("CREATE TABLE IF NOT EXISTS flashcards(id INTEGER PRIMARY KEY AUTOINCREMENT, stackId int, front varchar(350), reverse varchar(350))");
         _connection.Close();
     }
-    void Add()
+    public void Add(string front, string reverse, string stackName)
     {
+        var stack = stackController.GetBy(stackName, this);
+        Flashcard flashcard = new() { StackId = stack.Id, Front = front, Reverse = reverse };
+        _connection.Open();
+        _connection.Execute("INSERT INTO flashcards (front, reverse, stackId) VALUES($Front, $Reverse, $StackId)", flashcard);
+        _connection.Close();
 
     }
-
+    public IEnumerable<Flashcard> GetFlashcardsBy(string stackName)
+    {
+        ulong stackId = stackController.GetStackIdWith(stackName);
+        _connection.Open();
+        var flashcards = _connection.Query<Flashcard>("SELECT * FROM flashcards WHERE stackId = $Id", new { Id = stackId });
+        _connection.Close();
+        return flashcards;
+    }
 }
--- Controllers/StackController.cs
+++ Controllers/StackController.cs
@@ -12,21 +12,54 @@ class StackController(string dbConnection)
         _connection.Execute("CREATE TABLE IF NOT EXISTS stacks(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(50) UNIQUE)");
         _connection.Close();
     }
-    void Add(string name)
+    public void Add(string name)
     {
         if (Exists(name)) throw new ArgumentException($"The stack with name '{name}' already exist");
         FlashcardStack stack = new() { Name = name };
         _connection.Open();
-        _connection.Execute("INSERT INTO stacks (name) VALUES($Name)", name);
+        _connection.Execute("INSERT INTO stacks (name) VALUES($Name)", stack);
         _connection.Close();
-        Stacks.Add(stack);
     }
-    bool Exists(string name)
+    public bool Exists(string name)
     {
         _connection.Open();
-        ulong quantityOf = _connection.ExecuteScalar<ulong>("SELECT Count(*) FROM coding_session WHERE name = @Name ", new { Name = name });
+        ulong quantityOf = _connection.ExecuteScalar<ulong>("SELECT Count(*) FROM stacks WHERE name = $Name ", new { Name = name });
         _connection.Close();
         return quantityOf != 0;
     }
+    public void Delete(string name)
+    {
+        if (Exists(name) is false) throw new ArgumentException($"The stack with name '{name}' not exist");
+        _connection.Open();
+        _connection.Execute("DELETE FROM stacks WHERE name = $Name", new { Name = name });
+        _connection.Close();
+    }
+    public FlashcardStack GetBy(string name, FlashcardController flashcardController)
+    {
+        if (Exists(name) is false) throw new ArgumentException($"The stack with name '{name}' not exist");
+        _connection.Open();
+        var stack = _connection.QuerySingle<FlashcardStack>("Select id, name FROM stacks WHERE name = $Name", new { Name = name });
+        foreach (var flashcard in flashcardController.GetFlashcardsBy(name))
+        {
+            stack.Flashcards.Push(flashcard);
+        }
+
+
+        _connection.Close();
+        return stack;
+    }
+    public ulong GetStackIdWith(string name)
+    {
+        if (Exists(name) is false) throw new ArgumentException($"The stack with name '{name}' not exist");
+        _connection.Open();
+        var stack = _connection.QuerySingle<FlashcardStack>("Select id FROM stacks WHERE name = $Name", new { Name = name });
+        _connection.Close();
+        return stack.Id;
+    }
+    public void Modify()
+    {
+
+    }
+
 
 }
