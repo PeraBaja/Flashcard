79e977a4f695bf5b478555d4cea9dafd4aa05903
Benjamín Peralta <benjaminp158@gmail.com>
2025-03-31 13:49:59 -0300

    Added requiered keyword to id

--- Models/FlashcardDTO.cs
+++ Models/FlashcardDTO.cs
@@ -1,7 +1,7 @@
 namespace Flashcards.Models;
 class FlaschardDTO(Flashcard flashcard)
 {
-    public ulong Id;
+    public required ulong Id;
     public readonly string Front = flashcard.Front;
 
     public readonly string Reverse = flashcard.Reverse;
